c5afac9a6c2b6718e4224de3b31f25cb972a77ce
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-09-02 16:42:19 +0300

    more-mysql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,6 +2,7 @@
 -- id INT unique name VARCHAR(256) can’t be null
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(256) NOT NULL, UNIQUE (id),
   PRIMARY KEY (id)
